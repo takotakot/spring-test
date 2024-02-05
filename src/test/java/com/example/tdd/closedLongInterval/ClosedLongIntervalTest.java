1ced2253c1733c2acfdc1de9aa98080d9602dad8
takotakot <takotakot@users.noreply.github.com>
2024-02-05 22:02:22 +0900

    feat: Convert 閉区間[3, 8]に属する to a parameterized test

--- src/test/java/com/example/tdd/closedLongInterval/ClosedLongIntervalTest.java
+++ src/test/java/com/example/tdd/closedLongInterval/ClosedLongIntervalTest.java
@@ -6,6 +6,8 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import java.security.InvalidParameterException;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class ClosedLongIntervalTest {
   @Nested
@@ -32,9 +34,10 @@ public class ClosedLongIntervalTest {
   class _3_8_に属する {
     ClosedLongInterval interval = new ClosedLongInterval(3, 8);
 
-    @Test
-    void _3は閉区間_3_8_に属する() {
-      assertTrue(interval.contains(3));
+    @ParameterizedTest
+    @ValueSource(longs = {3, 5, 8})
+    void 閉区間_3_8_に属する(long value) {
+      assertTrue(interval.contains(value));
     }
   }
 }
